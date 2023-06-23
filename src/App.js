60a48e020436a917c6aedf890648ce0da91d945c
Kan <kanyakancat@gmail.com>
2023-06-23 13:47:43 +0300

    example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./main.global.css";
 import {Content} from "./components/Content";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Contact from "./Contact";
 
 function App() {
   return (
       <>
       <Content />
-      </>
+      <Link to="/" class="list">
+          Home
+      </Link>
+      <Link to="/contact" class="list">
+          Contact
+      </Link>
+
+    {/* Defining routes path and rendering components as element */}
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+    </Routes>
+</>
   );
 }
 export default App;
