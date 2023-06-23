46c6f6aa5a255db3a62e7aa60d5ab5c837936557
Kan <kanyakancat@gmail.com>
2023-06-23 15:31:53 +0300

    redirect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./main.global.css";
 import {Content} from "./components/Content";
-import { Link, Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import Contact from "./Contact";
 
 function App() {
-  return (
+    return (
       <>
       <Content />
       </>
