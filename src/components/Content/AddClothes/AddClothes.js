00737faa3b22f46ed02423adfadf8298474aa94b
Kan <kanyakancat@gmail.com>
2023-06-27 20:09:52 +0300

    added formik example

--- src/components/Content/AddClothes/AddClothes.js
+++ src/components/Content/AddClothes/AddClothes.js
@@ -4,23 +4,25 @@ import {Select} from "./Select/Select";
 import {CardImg} from "./CardImg";
 import {CardNote} from "../Card/CardElements/CardInfo/CardNote";
 import {Card} from "../Card";
+import {Formik, useFormik} from "formik";
 
 export function AddClothes() {
-
-    return <>
-        <Title value={'добавление вещи в гардероб'}/>
-        <ul>
-            <Card>
-            <div className={styles.container}>
-                <button className={styles.img}><CardImg /></button>
-                <div className={styles.inf}>
-                    <div className={styles.title}>НАЗВАНИЕ: <span className={styles.textarea}><CardNote/></span></div>
-                    <Select />
-                    <CardNote />
-                </div>
-            </div>
-        </Card>
-        </ul>
-
-    </>
-}
+        return (<>
+            <Title value={'добавление вещи в гардероб'}/>
+            <ul>
+                <Card>
+                    <form className={styles.container}>
+                        <button className={styles.img}><CardImg/></button>
+                        <div className={styles.inf}>
+                            <div className={styles.title}>НАЗВАНИЕ: <span
+                                    className={styles.textarea}><CardNote/></span></div>
+                                <Select/>
+                                <CardNote/>
+                                <br/>
+                                <input type="submit" value="Отправить"/>
+                        </div>
+                    </form>
+                </Card>
+            </ul>
+        </>)
+    }
--- src/components/Content/SelectClothes/SelectClothes.js
+++ src/components/Content/SelectClothes/SelectClothes.js
@@ -1,8 +1,81 @@
 import {Title} from "../Title/Title";
-
+import {Formik, useFormik} from "formik";
+import styles from './selectClothes.css'
+import {basicSchema} from "./schemas";
 export function SelectClothes() {
-         return <>
+
+    const onSubmit = () => {
+        console.log('submitted')
+    }
+
+    const {values, errors, touched, handleBlur, handleChange, handleSubmit} = useFormik( {
+        initialValues: {
+            email: '',
+            age:'',
+            password: '',
+            confirmPassword: ''
+        },
+        validationSchema: basicSchema,
+        onSubmit
+    })
+
+    console.log(errors)
+
+    return <>
         <Title value={'подбор одежды'}/>
-         </>
+
+            <form onSubmit={handleSubmit} className={styles.form} action="">
+                <label htmlFor="email">Email</label>
+                <input
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    type="email"
+                    id='email'
+                    placeholder='enter your email'
+                    className={errors.email && touched.email ? styles.inputError : ''}
+                />
+                <br/>
+                <label htmlFor="age">Age</label>
+                <input
+                    value={values.age}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    type="number"
+                    id='age'
+                    placeholder='enter your age'
+                    className={errors.age && touched.age ? styles.inputError : ''}
+
+                />
+                <br/>
+                <label htmlFor="password">Password</label>
+                <input
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    type="password"
+                    id='password'
+                    placeholder='enter your pass'
+                    className={errors.password && touched.password ? styles.inputError : ''}
+
+                />
+                <br/>
+                <label htmlFor="confirmPassword">Confirm password</label>
+                <input
+                    value={values.confirmPassword}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    type="password"
+                    id='confirmPassword'
+                    placeholder='confirm your pass'
+                    className={errors.confirmPassword && touched.confirmPassword ? styles.inputError : ''}
+
+                />
+                <br/>
+                <button type="submit">submit</button>
+
+
+            </form>
+    </>
 
 }
